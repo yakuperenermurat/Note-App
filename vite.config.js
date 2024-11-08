5c040f34c6d1b929553ddccc13a44897220aa4e4
yakuperenermurat <yakuperenermurat@gmail.com>
2024-11-08 14:52:39 +0300

    project update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',  // Bunu ekleyin
   build: {
     rollupOptions: {
       output: {
         entryFileNames: 'assets/[name].js',
         chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
-})
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
+});
